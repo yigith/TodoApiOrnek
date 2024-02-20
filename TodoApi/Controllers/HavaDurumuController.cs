6e825d0eb392cee76c84f6bb2928fcfb00b079f3
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2024-02-20 12:16:43 +0300

    Add project files.

---
+++ TodoApi/Controllers/HavaDurumuController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Dtos;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HavaDurumuController : ControllerBase
+    {
+        readonly Random _rnd = new();
+        readonly string[] _durumlar = { "Açık", "Parçalı Bulutlu", "Çok Bulutlu", "Sisli", "Puslu", "Yağmurlu", "Sağanak Yağışlı", "Kar Yağışlı", "Karla Karışık Yağmur", "Dolu", "Sıcak", "Soğuk", "Rüzgarlı", "Fırtına", "Kasırga" };
+
+        [HttpGet()]
+        public List<HavaDto> Get()
+        {
+            var tahminler = new List<HavaDto>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                tahminler.Add(new HavaDto()
+                {
+                    Tarih = DateOnly.FromDateTime(DateTime.Today.AddDays(i)),
+                    Sicaklik = _rnd.Next(-5, 30),
+                    Aciklama = _durumlar[_rnd.Next(_durumlar.Length)]
+                });
+            }
+
+            return tahminler;
+        }
+    }
+}
---
+++ TodoApi/Dtos/HavaDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApi.Dtos
+{
+    public class HavaDto
+    {
+        public DateOnly Tarih { get; set; }
+
+        public int Sicaklik { get; set; }
+
+        public string Aciklama { get; set; } = null!;
+    }
+}
